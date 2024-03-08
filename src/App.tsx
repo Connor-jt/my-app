6de1d61bf0b81157eaece1e3e5a02eaeff1d1aa3
Connor J Thompson <ct770a@gmail.com>
2024-03-09 00:57:08 +1100

    optimized reader is operational, accessing indexes is untested

--- src/App.tsx
+++ src/App.tsx
@@ -1,80 +1,138 @@
-import React from 'react';
+import React, { ChangeEventHandler } from 'react';
 import './App.css';
+import assert from 'assert';
+import { arrayBuffer } from 'stream/consumers';
 
 
-// ////////////////////////////////// //
+// ////////////////////////////////// // ----------------------------------------------------------------------------------
 // TEMP FILE LOADING FUNCTIONALITIES // ----------------------
-// //////////////////////////////// //
-
-// struct list of all open files
-// id
-// file
-// filename
-// file reader
+// //////////////////////////////// // ------------------------------------------------------------------------------------
 const FileChunkSize:number = 1024;
 class FileProcessor{
   file:File;
-  constructor(_file:File){
+  lowest_offset:number; // first byte of lower chunk
+  highest_offset:number; // last byte of upper chunk (unituitively this is actually the offset after the highest accessible byte, it just makes the code need a lot less '-1's)
+  lower_chunk:Uint8Array;
+  upper_chunk:Uint8Array;
+  has_init = false;
+ constructor(_file:File){
     this.file = _file;
-
-
-    // call first read to setup top and bottom chunks
-    this.read(0, 0);
+    this.lower_chunk = new Uint8Array();
+    this.upper_chunk = new Uint8Array();
+    this.lowest_offset = 0;
+    this.highest_offset = FileChunkSize*2;
   }
-  read(offset:number, count:number){
+  async init(){
+    this.lower_chunk = await this.call_file_read(0);
+    this.upper_chunk = await this.call_file_read(FileChunkSize);
+    this.has_init = true;
+    console.log(this.lower_chunk);
+    console.log(this.upper_chunk);
+    RefreshContentView();
+  }
+  async read(offset:number, count:number):Promise<Uint8Array>{
+    if (count != 0) throw new Error("dont read 0 bytes");
+    if (!this.has_init) throw new Error("reader uninitialized");
     if (count >= FileChunkSize) throw new Error("read size too large");
     // check to make sure at least some of the data falls inside
     if (offset < 0) throw new Error("byte offset below 0");
+    let last_offset = offset+count; // actually +1 of the last offset
     
-    // check whether the offset is out of range
+    // drop any overflowing indicies, so we're only attempting to read inside the boundaries of the file
+    { let bytes_overflowing = this.file.size - (offset + count);
+      if (bytes_overflowing > 0){
+        count -= bytes_overflowing
+        if (count >= 0) throw new Error("Attempting to read outside of file bounds");
+    }}
+
+    // check whether the offset is below currently loaded data
     if (offset < this.lowest_offset){
-      // then we need to read the previous chunk
-      
-      // check if the offset falls outside the range of the next block
-      if (offset - this.lowest_offset){
-
-      }
+      let chunk_lowest_offset = Math.trunc(offset / FileChunkSize) * FileChunkSize;
+      // if the data exists in the below block we can swap the current lowest chunk to the highest, else just read the upper chunk as well
+      if (offset - this.lowest_offset <= FileChunkSize) this.upper_chunk = this.lower_chunk;
+      else this.upper_chunk = await this.call_file_read(chunk_lowest_offset + FileChunkSize);
+      this.lower_chunk = await this.call_file_read(chunk_lowest_offset);
+      this.lowest_offset = chunk_lowest_offset;
+      this.highest_offset = chunk_lowest_offset + (FileChunkSize*2)
     }
-    if (offset + count > this.Highest_offset){
-      
+    // or if it is beyond the loaded data
+    else if (last_offset > this.highest_offset){
+      let upper_chunk_lowest_offset = Math.trunc(offset / FileChunkSize) * FileChunkSize;
+      // if the data exists in the above block we can swap the current highest chunk to the lowest, else just read the lowest chunk as well
+      if (this.highest_offset - last_offset <= FileChunkSize) this.lower_chunk = this.upper_chunk;
+      else this.lower_chunk = await this.call_file_read(upper_chunk_lowest_offset - FileChunkSize);
+      this.upper_chunk = await this.call_file_read(upper_chunk_lowest_offset);
+      this.lowest_offset = upper_chunk_lowest_offset - FileChunkSize;
+      this.highest_offset = upper_chunk_lowest_offset + FileChunkSize;
     }
+    
+    // if the data falls within the first chunk
+    let upper_chunk_offset = this.lowest_offset+FileChunkSize;
+    if (last_offset < upper_chunk_offset)
+      return this.lower_chunk.slice(offset - this.lowest_offset, count);
+    // if the data falls within the second chunk
+    else if (offset >= upper_chunk_offset)
+      return this.lower_chunk.slice(offset - upper_chunk_offset, count);
 
-    // drop any overflowing indicies
-
+    // else the data falls between the two, requiring merging the data
+    let bytes_from_lower = upper_chunk_offset - offset;
+    let merged = new Uint8Array(count);
+    merged.set(this.lower_chunk.slice(offset - this.lowest_offset, bytes_from_lower));
+    merged.set(this.lower_chunk.slice(0, count - bytes_from_lower));
+    return merged;
   }
-  prev_chunk(){
-
+  call_file_read(offset:number):Promise<Uint8Array>{
+    console.log("reading: " + offset.toString())
+    let owner = this;
+    function read_promise(){
+      return function(resolve: (arg0: Uint8Array) => void) {
+        const reader = new FileReader(); // should this be a static or class owned object??
+        reader.onload = function (e) {
+          const chunk = reader.result;
+          if (chunk == null) throw new Error("bad read request");
+          resolve(new Uint8Array(chunk as ArrayBuffer));
+        };
+        const blob = owner.file.slice(offset, offset + FileChunkSize); // this automatically trims the output size to whatever room it can possibly read
+        reader.readAsArrayBuffer(blob);
+    }}
+    return new Promise(read_promise());
   }
-  next_chunk(){
-
-  }
-
-  lowest_offset:number; // first byte of lower chunk
-  Highest_offset:number; // last byte of upper chunk
-
-  lower_chunk:Uint8Array;
-  upper_chunk:Uint8Array;
 }
+// -----------------------------------------------------------------------------------------------------------------------
+
+
 
 function hook_filesbox(){
 
-  var files_box = document.getElementById('file');
+  let files_box = document.getElementById('file');
   if (files_box == null) throw("failed to find file input element");
-  files_box!.onchange = e => { 
-      if (e.target == null) throw("filbox target doesn't exist");
-      var input = e.target as HTMLInputElement;
-      if (input.files == null || input.files!.length == 0) {return;} // "filbox no files selected"
-
-      var file:File = input.files![0];
-      if (file == null){return;} // dont do anything when we clear the file
-
-      
-      
-      
-
-
-  };
+  files_box!.onchange = e => {}
 }
+  
+  
+function FileBoxOnChanged(e: { target: HTMLInputElement | null; }){ 
+    if (e.target == null) throw("filebox target doesn't exist");
+    let input = e.target as HTMLInputElement;
+    if (input.files == null || input.files!.length == 0) {return;} // "filbox no files selected"
+
+    let file:File = input.files![0];
+    if (file == null){return;} // skip null file
+
+    let opened_file = new FileProcessor(file);
+    opened_file.init();
+    open_files.push(opened_file);
+};
+
+
+// //////////// //
+// GLOBAL DATA //
+// ////////// //
+var open_files:FileProcessor[] = [];
+
+
+
+
+
 
 // ///////////// //
 // UI FUNCTIONS //
@@ -130,6 +188,9 @@ function ContentView(){
   // need the "offset" and then offset per column and "decoded text"???
 
 }
+function RefreshContentView(){
+
+}
 
 // main app view
 function App() {
@@ -140,7 +201,7 @@ function App() {
         <button className='ToolItem'>File</button>
         <button className='ToolItem'>Edit</button>
         <button className='ToolItem'>Tools</button>
-        <input id="file" type="file" title="Select file to load" />
+        <input id="file" type="file" onChange={FileBoxOnChanged} title="Select file to load" />
       </div>
       {/* file view/list */}
       <div className='FileView'>
