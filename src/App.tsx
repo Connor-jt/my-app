1936719de212d4bc8f8f97c84f25783ac457a93a
Connor J Thompson <ct770a@gmail.com>
2024-03-10 18:00:08 +1100

    files are now opening and displaying hex

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,22 @@ import ReactDOM from 'react-dom';
 import { Root, createRoot } from 'react-dom/client';
 
 
+// ///////////////// // ------------------------------------------
+// HELPER FUNCTIONS //
+// /////////////// //
+function ToPaddedHex(value:number, digits:number) {
+  let hexString = value.toString(16);
+  return "0".repeat(Math.max(0, digits - hexString.length)) + hexString;
+}
+// Convert Uint8Array to hex string
+function Uint8ToHex(byte_array:Uint8Array) {
+  function i2hex(i:number) { return ('0' + i.toString(16)).slice(-2);}
+  return Array.from(byte_array).map(i2hex).join(' ');
+}
+// ---------------------------------------------------------------
+
+
+
 // ////////////////////////////////// // ----------------------------------------------------------------------------------
 // TEMP FILE LOADING FUNCTIONALITIES // ----------------------
 // //////////////////////////////// // ------------------------------------------------------------------------------------
@@ -33,7 +49,7 @@ class FileProcessor{
     RefreshFileView();
   }
   async read(offset:number, count:number):Promise<Uint8Array>{
-    if (count != 0) throw new Error("dont read 0 bytes");
+    if (count == 0) throw new Error("dont read 0 bytes");
     if (!this.has_init) throw new Error("reader uninitialized");
     if (count >= FileChunkSize) throw new Error("read size too large");
     // check to make sure at least some of the data falls inside
@@ -83,7 +99,7 @@ class FileProcessor{
     merged.set(this.lower_chunk.slice(0, count - bytes_from_lower));
     return merged;
   }
-  call_file_read(offset:number):Promise<Uint8Array>{
+  private call_file_read(offset:number):Promise<Uint8Array>{
     console.log("reading: " + offset.toString())
     let owner = this;
     function read_promise(){
@@ -128,83 +144,84 @@ var active_files:FileProcessor[] = []; // only modified when calling the refresh
 
 
 
+
+// ////////////// //
+// CONTENT VIEWS //
+// //////////// //
+var has_created_roots = false;
+var root_fileview_files:Root|undefined = undefined;
+var root_dataview_data:Root|undefined = undefined;
+var root_dataview_offsets:Root|undefined = undefined;
+function CheckRootHooks(){ // theres probably a way to do this after the document loads, but this is the best i can think of for now
+  if (has_created_roots) return;
+  root_fileview_files   = createRoot(document.getElementById('FilePanel')!);
+  root_dataview_data    = createRoot(document.getElementById('dataView')!);
+  root_dataview_offsets = createRoot(document.getElementById('offsetsView')!);
+  if (root_fileview_files == undefined)   throw new Error("Failed to find fileview offsets panel")
+  if (root_dataview_data == undefined)    throw new Error("Failed to find dataview offsets panel")
+  if (root_dataview_offsets == undefined) throw new Error("Failed to find offsetsview offsets panel")
+  has_created_roots = true;
+}
+
+function RefreshFileView(){
+  const FileViewContainer = () => {return(<div>{active_files.map((el, index) => React.createElement("button", { key: index, className: "ToolItem", onClick: FileClick, file_index: index }, el.file.name))}</div>);};
+  active_files = []; // first update the active files list, so our indexes correctly correspond
+  for (let i = 0; i < open_files.length; i++) if (open_files[i].has_init) active_files.push(open_files[i]);
+
+  CheckRootHooks();
+  root_fileview_files!.render(<FileViewContainer />);
+}
+function FileClick(e:Event){
+  if (e.target == null) throw("filebutton target doesn't exist");
+  let input = e.target as HTMLButtonElement;
+  // get the stored file index attribute
+  let index_property = input.getAttribute("file_index");
+  if (index_property == null) throw new Error()
+  let index = parseInt(index_property);
+
+  // do thing with this file index
+  // CHECK IF THIS FILE IS ALREADY OPEN
+  LoadBytesView(open_files[index])
+}
+
+// cached stuff for later use // so we dont have to regenerate all that data when we scroll up or down
+var dataview_offset_spans:HTMLSpanElement[] = [];
+var dataview_byte_spans:HTMLSpanElement[] = [];
+async function LoadBytesView(file:FileProcessor){
+  // read & convert all bytes into react span elements
+  let byte_spans:React.DetailedReactHTMLElement<{key: number;className: string;}, HTMLElement>[] = [];
+  let offset_spans:React.DetailedReactHTMLElement<{key: number;className: string;}, HTMLElement>[] = [];
+  for (let line_index = 0; line_index < total_rows; line_index++){
+    let row_bytes = await file.read(line_index*bytes_per_row, bytes_per_row);
+    var row_text_content = Uint8ToHex(row_bytes);
+    byte_spans.push(React.createElement("span", { key: line_index, className: "DataSpan" }, row_text_content));
+    offset_spans.push(React.createElement("span", { key: line_index, className: "DataSpan" }, ToPaddedHex(line_index*bytes_per_row, 8)));
+  }
+  // then render data
+  CheckRootHooks();
+  const DataViewContainer = () => {return(<div>{byte_spans}</div>);};
+  const OffsetsViewContainer = () => {return(<div>{offset_spans}</div>);};
+  root_dataview_data!.render(<DataViewContainer />);
+  root_dataview_offsets!.render(<OffsetsViewContainer />);
+}
+
 // ///////////// //
 // UI FUNCTIONS //
 // /////////// //
 const line_height = 18;
 const offset_bytes = 4;
 var total_bytes:number = NaN;
-var bytes_per_row:number = NaN;
+var bytes_per_row:number = 10;
 
 var available_size:number = 0;
 var visible_rows:number = NaN;
-var total_rows:number = NaN;
+var total_rows:number = 5;
 
 var last_row_index:number = NaN;
 var last_row_count:number = NaN;
 
 var rows_added_top:number = NaN; // added/removed from the top of the data view
 var rows_added_bot:number = NaN; // added/removed from bottom
-
-
-function update_onscreen_data(){
-
-}
-function update_data_scroll(){
-  // calculate how much of the screen space we're currently viewing
-
-}
-
-function UpdateOffsetView(){
-  // get available 
-}
-
-
-// ////////////// //
-// CONTENT VIEWS //
-// //////////// //
-function DataView(){
-  
-}
-// used to search/find values from any 
-function SearchView(){
-
-}
-// used to view/edit any highlighted data from any dataview
-function EditView(){
-
-}
-
-// wrapper window for files and content views
-function FileView(){
-
-  // need the offset side bar
-  // need the "offset" and then offset per column and "decoded text"???
-
-}
-var fileview_root:Root|undefined = undefined;
-function RefreshFileView(){
-  const FileViewContainer = () => {return(<div>{active_files.map((el, index) => React.createElement("button", { key: index, className: "ToolItem", onClick: FileClick, file_index: index }, el.file.name))}</div>);};
-  // first update the active files list, so our indexes correctly correspond
-  active_files = [];
-  for (let i = 0; i < open_files.length; i++)
-    if (open_files[i].has_init) active_files.push(open_files[i]);
-
-  if (fileview_root == null) fileview_root = createRoot(document.getElementById('FilePanel')!);
-  fileview_root.render(<FileViewContainer />);
-}
-function FileClick(e:Event){
-  if (e.target == null) throw("filebutton target doesn't exist");
-  let input = e.target as HTMLButtonElement;
-  // get the stored file index attribute
-  let index_property = input.getAttribute("file_index") as string;
-  if (index_property == null) throw new Error()
-  let index = parseInt(index_property);
-
-  // do thing with this file index
-  console.log(index);
-  console.log(open_files[index]);
-}
 
 // main app view
 function App() {
@@ -226,7 +243,8 @@ function App() {
       <hr className='FileViewSeparator'></hr>
       {/* content views */}
       <div className='ContentView'>
-        <div className='OffsetView'>00000000</div>
+        <div id="offsetsView" className='OffsetView'>00000000</div>
+        <div id="dataView" className='DataView'>00000000</div>
       </div>
       <div className='Footer'/>
     </div>
